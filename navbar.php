885677fb131e568508eadc6796494fe133283e20
amin <wahidhidayat517@gmail.com>
2021-02-09 11:11:01 +0700

    a

---
+++ navbar.php
@@ -0,0 +1,44 @@
+<link href="bootstrap-3.3.7-dist/css/bootstrap.css" rel="stylesheet" type="text/css">
+  <script src="bootstrap-3.3.7-dist/js/jquery-3.0.0.js"></script>
+  <script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+  <link rel="stylesheet" type="text/css" href="cob.css">
+
+<div class="container-fluid">
+<div class="row">
+	<div class="col-sm-12">
+	<nav class="nav navbar navbar-default navbar-fixed-top" style="width:80%;margin-left:10%;">
+		<div class="navbar-header"><a class="navbar-brand" href="#">AMIENZ</a></div>
+	<nav class="nav navbar navbar-inverse navbar-fixed-top" style="width:80%;margin-left:10%;">
+		<div class="navbar-header"><a class="navbar-brand" href="#">AMIENZ</a></div>
+			<ul class="nav navbar-nav">
+				<li class="dropdown"><a href="#">TV Series
+					<ul class="dropdown-menu">
+						<li style="margin-top:-10px;"><a href="#">Ongoing</a></li>
+						<li><a href="#">End</a></li>
+					</ul>
+				</li>
+				<li class="dropdown"><a href="#">Movie
+					<ul class="dropdown-menu">
+						<li style="margin-top:-10px;"><a href="#">Ongoing</a></li>
+						<li><a href="#">End</a></li>
+					</ul>
+				</li>
+				<li><a data-toggle="dropdown" href="#">HOT</a></li>
+				<li><a data-toggle="dropdown" href="#">News</a></li>
+				<li><a data-toggle="dropdown" href="#">About</a></li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+				<li style="margin-right:10px;"><a href="#" data-toggle="modal" data-target="#mymodal">
+				<span class="glyphicon glyphicon-user"></span>Log In</a></li>
+			</ul>			
+			<form class="navbar-form navbar-right">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search . .">
+				</div>
+			</form>
+	</nav>
+	</div>
+<br><br><br>
+</div>
+</div>
