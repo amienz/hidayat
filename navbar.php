fb9283e5b5017a6a7f59c337b3f7b35c0b117a17
amin <wahidhidayat517@gmail.com>
2021-02-10 10:40:34 +0700

    a

--- navbar.php
+++ navbar.php
@@ -25,9 +25,6 @@
 				</li>
 				<li><a data-toggle="dropdown" href="#">HOT</a></li>
 				<li><a data-toggle="dropdown" href="#">News</a></li>
-
-
-
 			</ul>
 			
 			<ul class="nav navbar-nav navbar-right">
