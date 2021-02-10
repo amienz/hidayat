ed1ba983be137e1ccd2bcc6c99ef9c865a3b241e
amin <wahidhidayat517@gmail.com>
2021-02-10 16:43:38 +0700

    a

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
 		</div>
 	
 		<div class="item">
-			<img src="gambar/saske2.jpg" class="img-rounded " alt="saske">
+			<img src="gambar/kirayagami.jpg" class="img-rounded " alt="saske">
 				<div class="carousel-caption">
 					<h3>Saske</h3>
 					<p>Mangekyou sharingan for activated susanoo level 3</p>
